83a336e06f93c144fdb742da24baa78a02855504
Christian Ramirez <christian.ramirez@patelhs.org>
2020-09-30 14:40:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+   int myAge=15;
+   int myBirthday=22;
+   int todaysDate=21;
+  
+   if(myBirthday==todaysDate)
+  {
+   myAge++;
+   System.out.println("I am "+myAge+" yearsold");
+  }else{
+   System.out.println("I am still "+myAge+" yearsold");
+  }
+  }
+}  
